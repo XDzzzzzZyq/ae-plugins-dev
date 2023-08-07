7d3e663705ba8684b86ec9184866afb5c0e5adda
XDzZyq <xiaodouzizyq@gmail.com>
2023-08-07 14:31:47 +0800

    8.7 invert path detact

--- dep/core/xdz_math.h
+++ dep/core/xdz_math.h
@@ -1,5 +1,6 @@
 
 #include "glm/glm.hpp"
+#include "glm/gtx/exterior_product.hpp"
 
 constexpr bool operator<(const glm::vec2& _a, const glm::vec2& _b) {
 	return _a.x < _b.x || _a.y < _b.y;
@@ -21,4 +22,17 @@ inline double random01(unsigned int seed) {
 #define PI 3.14159265358979323846
 inline double hash01(double in, double seed) noexcept {
 	return fmod(in*seed*1.145141919810, PI) / PI;
+}
+
+#include <vector>
+inline bool is_invert(const std::vector<glm::vec2>& verts) {
+	float dir = 0;
+	for (int i = 0; i < verts.size(); i++) {
+		const glm::vec2 v1 = verts[(i + 1) % verts.size()] - verts[i];
+		const glm::vec2 v2 = verts[(i + 2) % verts.size()] - verts[(i + 1) % verts.size()];
+
+		dir += glm::asin(glm::cross(glm::normalize(v1), glm::normalize(v2)));
+	}
+
+	return dir > 0;
 }
